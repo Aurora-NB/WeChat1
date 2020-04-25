5e3ea0a671a36d3d62f8a3e161290c1b11337847
Aurora-NB <772335766@qq.com>
2020-04-25 22:43:08 +0800

    改名改配置

---
+++ pages/Individuals/Individuals.js
@@ -0,0 +1,40 @@
+Page({
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  onReady: function () {
+    
+  },
+
+  onShow: function () {
+    
+  },
+
+  onHide: function () {
+    
+  },
+
+  onUnload: function () {
+    
+  },
+
+  onPullDownRefresh: function () {
+    
+  },
+
+  onReachBottom: function () {
+    
+  },
+
+  onShareAppMessage: function () {
+    
+  }
+})
